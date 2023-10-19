04494500aaf7b999bb74b558f2ec91fd1a595f8e
Mozzammel Hossen <53122797+abirpuc@users.noreply.github.com>
2023-10-19 09:46:06 +0600

    Upload the projects

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Footer from './sharedComponents/Footer'
+import Topnav from './sharedComponents/Topnav'
+
+
+function App() {
+
+  return (
+    <section>
+      <header>
+        <Topnav />
+      </header>
+      <main className='min-h-[100vh]'>
+
+      </main>
+      <div>
+        <Footer />
+      </div>
+    </section>
+  )
+}
+
+export default App
